4fba8ec55a95536f9be335e41ad40d0f07d6cd22
James Neufeld <jamesdneufeld@gmail.com>
2025-09-11 17:08:03 -0700

    overlay added

--- js/javascript.js
+++ js/javascript.js
@@ -1,40 +1,34 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
-    // Alternative animation for example
-    // slideToggle("fast");
-    $(document).ready(function(){
-	$('#navigation-item-1').hover(function(){
-  		$('.overlay1').fadeIn(500);
-  	},function(){
-  		$('.overlay1').fadeOut(500);
-  });
-	$('#navigation-item-2').hover(function(){
-  		$('.overlay2').fadeIn(500);
-  	},function(){
-  		$('.overlay2').fadeOut(500);
-  });
-	$('#navigation-item-3').hover(function(){
-  		$('.overlay3').fadeIn(500);
-  	},function(){
-  		$('.overlay3').fadeOut(500);
-  });  
-  $('#navigation-item-4').hover(function(){
-  		$('.overlay3').fadeIn(500);
-  	},function(){
-  		$('.overlay3').fadeOut(500);
-  });  
-  $('#navigation-item-5').hover(function(){
-  		$('.overlay3').fadeIn(500);
-  	},function(){
-  		$('.overlay3').fadeOut(500);
-  });
-  $('#navigation-item-6').hover(function(){
-  		$('.overlay3').fadeIn(500);
-  	},function(){
-  		$('.overlay3').fadeOut(500);
-  });  
-});
-
-  });
+$(document).ready(function () {
+  $("#navigation-item-1").hover(
+    function () {
+      $(".overlay1").fadeIn(500);
+    },
+    function () {
+      $(".overlay1").fadeOut(500);
+    }
+  );
+  $("#navigation-item-2").hover(
+    function () {
+      $(".overlay2").fadeIn(500);
+    },
+    function () {
+      $(".overlay2").fadeOut(500);
+    }
+  );
+  $("#navigation-item-3").hover(
+    function () {
+      $(".overlay3").fadeIn(500);
+    },
+    function () {
+      $(".overlay3").fadeOut(500);
+    }
+  );
+  $("#navigation-item-4").hover(
+    function () {
+      $(".overlay4").fadeIn(500);
+    },
+    function () {
+      $(".overlay4").fadeOut(500);
+    }
+  );
 });
