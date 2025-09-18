fd0b9080ed0ffbbc44350e062650ad21dcea947c
ukkop <andypoystila22@gmail.com>
2025-09-18 16:28:47 -0700

    added draggabledivs but they dont drag yet

--- js/draggabledivscript.js
+++ js/draggabledivscript.js
@@ -1,50 +1,46 @@
-$(document).ready(function () {
-  $("#navigation-item-1").hover(
-    function () {
-      $(".overlay1").fadeIn(500);
-    },
-    function () {
-      $(".overlay1").fadeOut(500);
-    }
-  );
-  $("#navigation-item-2").hover(
-    function () {
-      $(".overlay2").fadeIn(500);
-    },
-    function () {
-      $(".overlay2").fadeOut(500);
-    }
-  );
-  $("#navigation-item-3").hover(
-    function () {
-      $(".overlay3").fadeIn(500);
-    },
-    function () {
-      $(".overlay3").fadeOut(500);
-    }
-  );
-  $("#navigation-item-4").hover(
-    function () {
-      $(".overlay4").fadeIn(500);
-    },
-    function () {
-      $(".overlay4").fadeOut(500);
-    }
-  );
-  $("#navigation-item-5").hover(
-    function () {
-      $(".overlay5").fadeIn(500);
-    },
-    function () {
-      $(".overlay5").fadeOut(500);
-    }
-  );
-  $("#navigation-item-6").hover(
-    function () {
-      $(".overlay6").fadeIn(500);
-    },
-    function () {
-      $(".overlay6").fadeOut(500);
-    }
-  );
-});
+// Make the DIV element draggable:
+dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
