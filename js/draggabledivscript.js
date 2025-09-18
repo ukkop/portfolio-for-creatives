5fdc31be86883010ae79ba9a02420bea1487d9d2
ukkop <andypoystila22@gmail.com>
2025-09-18 16:17:11 -0700

    created draggabledivscript

---
+++ js/draggabledivscript.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  $("#navigation-item-1").hover(
+    function () {
+      $(".overlay1").fadeIn(500);
+    },
+    function () {
+      $(".overlay1").fadeOut(500);
+    }
+  );
+  $("#navigation-item-2").hover(
+    function () {
+      $(".overlay2").fadeIn(500);
+    },
+    function () {
+      $(".overlay2").fadeOut(500);
+    }
+  );
+  $("#navigation-item-3").hover(
+    function () {
+      $(".overlay3").fadeIn(500);
+    },
+    function () {
+      $(".overlay3").fadeOut(500);
+    }
+  );
+  $("#navigation-item-4").hover(
+    function () {
+      $(".overlay4").fadeIn(500);
+    },
+    function () {
+      $(".overlay4").fadeOut(500);
+    }
+  );
+  $("#navigation-item-5").hover(
+    function () {
+      $(".overlay5").fadeIn(500);
+    },
+    function () {
+      $(".overlay5").fadeOut(500);
+    }
+  );
+  $("#navigation-item-6").hover(
+    function () {
+      $(".overlay6").fadeIn(500);
+    },
+    function () {
+      $(".overlay6").fadeOut(500);
+    }
+  );
+});
